82bb5b2ccdf1b32e8f6aca78179dd891f68ccc64
Darejk <nguyendaian1945@gmail.com>
2024-12-18 00:15:26 +0700

    update i18

--- src/Localization/languages/en.ts
+++ src/Localization/languages/en.ts
@@ -6,4 +6,29 @@ export const en = {
   [LocalizationKey.HOME]: "Home",
   [LocalizationKey.GET_STARTED]: "Get Started",
   [LocalizationKey.LOADING]: "Loading",
+
+  // Login
+  [LocalizationKey.LOGIN_SUCCESS]: "Login Successful",
+  [LocalizationKey.LOGIN_FAILED]: "Login Failed",
+  [LocalizationKey.EMAIL_PLACEHOLDER]: "Enter your email",
+  [LocalizationKey.PASSWORD_PLACEHOLDER]: "Enter your password",
+  [LocalizationKey.LOGIN]: "Login",
+
+  // Register
+  [LocalizationKey.REGISTER]: "Register",
+  [LocalizationKey.REGISTER_SUCCESS]: "Registration Successful",
+  [LocalizationKey.REGISTER_FAILED]: "Registration Failed",
+  [LocalizationKey.USERNAME_PLACEHOLDER]: "Enter your username",
+  [LocalizationKey.NAME_PLACEHOLDER]: "Enter your name",
+
+  // Avatar
+  [LocalizationKey.LINK_AVATAR_DEFAULT]: "Default Avatar Link",
+
+  // Switch between Login/Register
+  [LocalizationKey.SWITCH_TO_LOGIN]: "Already have an account? Login",
+  [LocalizationKey.SWITCH_TO_REGISTER]: "Don't have an account? Register",
+
+  // Titles
+  [LocalizationKey.LOGIN_TITLE]: "Login to Your Account",
+  [LocalizationKey.REGISTER_TITLE]: "Create a New Account",
 };
